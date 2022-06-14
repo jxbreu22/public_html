f818a7b7ed2b994e5f4402bfbe7255cab1e46cf1
El Bartsch <jxbreu@rit.edu>
2022-06-14 15:19:07 -0400

    testing data scripts

---
+++ Code/js/submit.php
@@ -0,0 +1,11 @@
+<?php
+ 
+ header("Content-Type: application/json");
+  
+ $data = json_decode(file_get_contents("php://input"));
+  
+ $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+ fwrite($myfile, $data);
+ fclose($myfile);
+  
+ ?>
